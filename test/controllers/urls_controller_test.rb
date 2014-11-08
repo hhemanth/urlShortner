2aac8bdae55d567cec957d6220b9b6933a56f7f8
Hemanth Haridas <hemanth.haridas@filpkart.com>
2014-11-08 15:55:55 +0530

    renamed controller from url to urls

--- test/controllers/urls_controller_test.rb
+++ test/controllers/urls_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class UrlControllerTest < ActionController::TestCase
+class UrlsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
